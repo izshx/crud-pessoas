3aaa130b1e543c5a4e29a6ebe5efc2e02bd4be81
izephyx <maick_cs2@hotmail.com>
2019-10-27 00:55:50 -0300

    Alterações no app componente para alinhar com crud pessoas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
+import { PessoaService } from './pessoa.service';
+import { CadPessoaComponent } from './cad-pessoa/cad-pessoa.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPessoaComponent,
+    CadPessoaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    PessoaService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
